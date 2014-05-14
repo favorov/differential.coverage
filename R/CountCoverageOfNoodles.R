30d8dd8317d2c462d225db1e3de3dc92636e3076
Alexander Favorov <favorov@sensi.org>
2014-05-14 02:55:58 -0400

    dumb bed.id cycle

--- R/CountCoverageOfNoodles.R
+++ R/CountCoverageOfNoodles.R
@@ -1,2 +1,12 @@
-CountCoverageOfNoodles<-function(noodles,bedfilnames){
+CountCoverageOfNoodles<-function(noodles,bedfilnames,bed.ids=bedfilnames){
+	if (length(bedfilnames)!=length(bed.ids))
+	{
+		stop("The lists of bed file names and bed ids has different lenghts. So what?")
+	}
+	else
+		names(bedfilnames)=bed.ids
+	for (bed.id in bed.ids)
+	{
+		print(bed.id)
+	}
 }
