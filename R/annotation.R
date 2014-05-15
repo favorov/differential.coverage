561070d41927b9809a7254eb381c8e078f0a4327
Alexander Favorov <favorov@sensi.org>
2014-05-15 18:49:58 -0400

    nulleic chromosomes only

--- R/annotation.R
+++ R/annotation.R
@@ -1,4 +1,11 @@
-chromosomes.hg19<-function()
+#Differential.Coverage library
+#A. Favorov, S. Wheelan 2014
+#annotation utilities
+
+nucl.chromosomes.hg19<-function()
 {
-	seqinfo(TxDb.Hsapiens.UCSC.hg19.knownGene)
+	chr.all<-seqinfo(TxDb.Hsapiens.UCSC.hg19.knownGene)
+	chr.all[names(chr.all[1:24])]
+	#only chrNN, chrX and chrY.
+	#chrM is 25
 }
--- R/prepare_noodles.R
+++ R/prepare_noodles.R
@@ -1,2 +1,11 @@
+#Differential.Coverage library
+#A. Favorov, S. Wheelan 2014
 #prepare noodles
 
+#returns noodles that uniformly cover
+#the set of chromosomes that are described by 
+#sequinfo
+prepare.uniform.noodle<-function(seq.info)
+{
+	print('nooooddlee')
+}
