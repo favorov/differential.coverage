799f96c7e61eadd5d9102dfe3fb2092cfaf07d3d
Alexander Favorov <favorov@sensi.org>
2014-05-15 23:25:29 -0400

    uniform noodle generator works

--- R/annotation.R
+++ R/annotation.R
@@ -5,7 +5,7 @@
 nucl.chromosomes.hg19<-function()
 {
 	chr.all<-seqinfo(TxDb.Hsapiens.UCSC.hg19.knownGene)
-	chr.all[names(chr.all[1:24])]
+	chr.all[names(chr.all)[1:24]]
 	#only chrNN, chrX and chrY.
 	#chrM is 25
 }
--- R/prepare_noodles.R
+++ R/prepare_noodles.R
@@ -5,7 +5,38 @@
 #returns noodles that uniformly cover
 #the set of chromosomes that are described by 
 #sequinfo
-prepare.uniform.noodles<-function(seq.info)
+#seqinfo is seqinfo or a vector of ints with the names as chromosome.names
+prepare.uniform.noodles<-function(seq.info,noodle.length=1000)
 {
-	print('nooooddlee')
+	
+	if (class(seq.info) == 'Seqinfo')
+		seq.info<-seqlengths(seq.info)
+	#now, seqinfo is a vector of ints with the names
+	noodles.seqnames=character(0)
+	noodles.start=integer(0)
+	noodles.end=integer(0)
+	message('generating noodles')
+	for (chr in names(seq.info))
+	{
+		this.noodles.start<-seq(1,seq.info[chr],by=noodle.length)
+		noodles.start<-c(noodles.start,this.noodles.start)
+		
+		noodles.seqnames<-c(noodles.seqnames,rep(chr,length(this.noodles.start)))
+		
+		this.noodles.end<-this.noodles.start+noodle.length-1
+		this.noodles.end[length(this.noodles.end)]<-
+			min(this.noodles.end[length(this.noodles.end)],seq.info[chr])
+		#for the last noodle not to break end of chr
+		noodles.end<-c(noodles.end,this.noodles.end)
+	}
+	message('combining noodles')
+	GRanges(
+		seqlengths=seq.info,
+		seqnames=noodles.seqnames,
+		ranges=IRanges
+		(
+			start=noodles.start,
+			end=noodles.end
+		)
+	)
 }
