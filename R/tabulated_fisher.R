dd764d9e14f9175668b1da267e7aaf803d2a6317
Alexander Favorov <favorov@sensi.org>
2014-06-19 02:57:33 -0400

    started to write the tabulated Fisher

---
+++ R/tabulated_fisher.R
@@ -0,0 +1,15 @@
+#Differential.Coverage library
+#A. Favorov, E. Fertig, S. Wheelan 2014
+#tabulated Fisher
+
+#parameters: folder (default .)
+#Y
+#N
+#theese two are numbers of positive and negative samples.
+#we form the file as tabulated.fisher.Y.N.dat
+#the dataframe tabulated.fisher
+#each line corresponds for 4-pole table:
+# MY    MN 
+#MY-MY N-MN
+#MY is methylated-positive  and MN is methylated-negative
+#rownumber = (N-1) MY + MN (standard 2-D transform)
