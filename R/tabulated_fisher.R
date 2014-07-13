6caaa04a6c47fecc6e3ce21522e7d80b15597a6d
Alexander Favorov <favorov@sensi.org>
2014-07-13 04:37:11 +0400

    a ctitical addressing bug fixed (N instead of N+1 in multiplier) fixed

--- R/tabulated_fisher.R
+++ R/tabulated_fisher.R
@@ -3,16 +3,26 @@
 #tabulated Fisher
 
 #parameters: folder (default .)
-#Y
+#Y 
 #N
-#theese two are numbers of positive and negative samples.
+#theese two are numbers of positive (e.g. cancer) and negative (normal) samples.
 #we form the file as tabulated.fisher.Y.N.dat
 #the dataframe tabulated.fisher
 #each line corresponds for 4-pole table:
 # MY    MN 
-#MY-MY N-MN
-#MY is methylated-positive  and MN is methylated-negative
-#rownumber = N * MY + MN + 1 (standard 2-D transform)
+# Y-MY N-MN
+#MY is methylated-positive and MN is methylated-negative (normal)
+#we want to represent all the Fishers as a (Y+1) rows * (N+1) cols
+#matrix.
+#MY=0..Y
+#MN=0..N
+#then, we map it to 1..(MY+1)(MN+1)
+#
+
+tab.fisher.row.no<-function(Y,N,MY,MN)
+{
+	(N+1)*MY+MN+1	
+}
 
 prepare.tabulated.fisher<-function(Y,N,folder='.',load=TRUE,save=TRUE)
 {
@@ -41,15 +51,14 @@ prepare.tabulated.fisher<-function(Y,N,folder='.',load=TRUE,save=TRUE)
 	for (MY in 0:Y)
 		for (MN in 0:N)
 		{
-			cotable<-matrix(c(MY,Y-MY,MN,N-MN),ncol=2)
+			cotable<-matrix(c(MY,MN,Y-MY,N-MN),ncol=2)
 			fisherres<-fisher.test(cotable)
-			tabulated.fisher[N*MY+MN+1,]<-c(fisherres$p.value,cotable[2,2]/cotable[1,2],cotable[2,1]/cotable[1,1],fisherres$estimate,fisherres$conf.int[1],fisherres$conf.int[2])
+			tabulated.fisher[tab.fisher.row.no(Y,N,MY,MN),]<-c(fisherres$p.value,cotable[2,2]/cotable[1,2],cotable[2,1]/cotable[1,1],fisherres$estimate,fisherres$conf.int[1],fisherres$conf.int[2])
 		}	
 	#print(dim(tabulated.fisher))
 	message('saving fisher tabulation')
 	if(save)
 		save(file=Rda.name,list=c('tabulated.fisher','Y','N'))
-		
 	return (tabulated.fisher) 	
 }
 
