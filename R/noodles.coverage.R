84d12dd96d0596f28b3d68321140018127dfa566
Alexander Favorov <favorov@sensi.org>
2016-01-03 03:30:37 -0500

    new function max.peak.score.for.each.noodle added

--- R/noodles.coverage.R
+++ R/noodles.coverage.R
@@ -43,16 +43,18 @@ count.coverage.of.noodles<-function(noodles,bedfilnames,bed.ids=bedfilnames){
 	noodles.coverage
 }
 
-#it counts and return noodles.coverage data frame
+CountCoverageOfNoodles<-function(noodles,bedfilnames,bed.ids=bedfilnames){
+	.Deprecated('count.coverage.of.noodles')
+	count.coverage.of.noodles(noodles,bedfilnames,bed.ids)
+}
+
 #'indicate.any.coverage.of.noodles
 #'
 #'It is the alterantive central function of all the differential.coverage package. It gets a set of intervals (noodles) as a \code{GRanges} and a list of names of bed files.
 #'Each bedfile represents a sample. For each noodle and each sample, 0 or 1, which indicates whether the noodle intersects with any bed interval from the sample, is returned.
 #'
-#'@param noodles \code{GRanges} with the intervals
-#'@param bedfilnames list of names of bedfiles, one per sample, with some (e.g. methylation) coverage information
-#'@param bed.ids optional list of names for the samples, they will be used as column names in the result. Default = \code{bedfilnames}
-#'@return \code{Matrix}, each row correspond to a noodle; columns are samples, sparse=TRUE
+#'@inheritParams count.coverage.of.noodles
+#'@return \code{Matrix}, each row correspond to a noodle; columns are samples, sparse=TRUE, value=0 or 1 
 #'@seealso \code{differential.coverage}
 indicate.any.coverage.of.noodles<-function(noodles,bedfilnames,bed.ids=bedfilnames){
 	if (class(noodles)!='GRanges')
@@ -78,7 +80,38 @@ indicate.any.coverage.of.noodles<-function(noodles,bedfilnames,bed.ids=bedfilnam
 	noodles.coverage
 }
 
-CountCoverageOfNoodles<-function(noodles,bedfilnames,bed.ids=bedfilnames){
-	.Deprecated('count.coverage.of.noodles')
-	count.coverage.of.noodles(noodles,bedfilnames,bed.ids)
+
+#'max.peak.score.for.each.noodle
+#'
+#'Could be an alterantive central function of all the differential.coverage package. It gets a set of intervals (noodles) as a \code{GRanges} and a list of names of bed files.
+#'Each bedfile represents a sample. For each noodle and each sample, the maximal score of a peak that intersect this noodle in the sample, if any, or 0 othewise, is returned. 
+#'Supposed to called for annotation of a few selected noodles.
+#'@inheritParams count.coverage.of.noodles
+#'@return \code{Matrix}, each row correspond to a noodle; columns are samples, sparse=TRUE, value=max(score) or 0
+#'@seealso \code{differential.coverage}
+max.peak.score.for.each.noodle<-function(noodles,bedfilnames,bed.ids=bedfilnames){
+	if (class(noodles)!='GRanges')
+	{
+		stop("The noodles for coverage is not GRanges. So what?")
+	}
+	if (length(bedfilnames)!=length(bed.ids))
+	{
+		stop("The lists of bed file names and bed ids has different lenghts. So what?")
+	}
+	else
+		names(bedfilnames)<-bed.ids
+	message('coverage')
+	noodles.coverage<-Matrix(0,ncol=length(bed.ids),nrow=length(noodles),sparse = TRUE)
+	colnames(noodles.coverage)<-bed.ids
+	for (bed.id in bed.ids)
+	{
+		message(bed.id)
+		beads<-import(bedfilnames[bed.id])
+		overrle<-findOverlaps(noodles,beads)
+		#beads$score[subjectHits(overrle) is a vector of scores on intersected peaks
+		maxi<-tapply(beads$score[subjectHits(overrle)],queryHits(overrle),max)
+		noodles.max.scores[as.integer(names(covered)),bed.id]<-maxi
+	}
+	noodles.max.scores
 }
+
