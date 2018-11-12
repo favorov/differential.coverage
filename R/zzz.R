2893cc95d07396f6230f18b364b88db97fa3a9e2
Alexander Favorov <favorov@sensi.org>
2018-11-12 00:23:54 -0500

    version 0.1.7

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
        version <- packageDescription("differential.coverage", field="Version")
-       packageStartupMessage(paste("Welcome to differential.coverage version ", version),", St. Patrick's day version!")
+       packageStartupMessage(paste("Welcome to differential.coverage version ", version),", Winter is Coming version!")
 }
