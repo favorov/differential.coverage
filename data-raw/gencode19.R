3710d06b08f500f46cd5f671e5eee2b3ea80bbc0
Alexander Favorov <favorov@sensi.org>
2019-01-25 00:52:36 -0500

    gencode19 ok

--- data-raw/gencode19.R
+++ data-raw/gencode19.R
@@ -1,5 +1,6 @@
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(rtracklayer)
+library(dplyr)
 library(usethis)
 
 if(! 'raw_differtial_coverage_gencode_19_gff' %in% ls()){
@@ -16,8 +17,10 @@ seqs<-seqs[seqnms] #seqinfo is subesettable only by names, we make useful chromo
 gencode19_genes<-
   GRanges(as.data.frame(raw_differtial_coverage_gencode_19_gff) %>% 
       filter(type=="gene") %>% 
-      select(seqid,start,end,SYMBOL=gene_name,GENE_ID=gene_id),
+      select(seqid,start,end,strand,gene_name,ensembl=gene_id),
   seqinfo = seqs)
+#gene_id here is ensembl name, but withe additional .n (transcript#), we remove it
+gencode19_genes$ensembl<-sapply(strsplit(gencode19_genes$ensembl,'.',fixed = TRUE),"[[",1)
 
 #we are in the package root folder
 use_data(gencode19_genes,overwrite = TRUE)
