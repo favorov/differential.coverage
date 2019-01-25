2bee5081bd6d4b611e84af38589d79109e325112
Alexander Favorov <favorov@sensi.org>
2019-01-25 00:52:52 -0500

    going on...

--- data-raw/ucsc_hg19.R
+++ data-raw/ucsc_hg19.R
@@ -1,7 +1,9 @@
 library(org.Hs.eg.db)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(usethis)
 
 suppressMessages(
-  geneSymbols.by.ENTEZId <- select(
+  geneSymbols.by.ENTEZId <- AnnotationDbi::select(
   org.Hs.eg.db,
   keys=keys(org.Hs.eg.db,keytype = 'ENTREZID'),
   columns=c('SYMBOL'),
@@ -10,6 +12,10 @@ suppressMessages(
 )
 
 rownames(geneSymbols.by.ENTEZId)=geneSymbols.by.ENTEZId[,1]
+
+genes<-genes(TxDb.Hsapiens.UCSC.hg19.knownGene,single.strand.genes.only=FALSE)
+
+
 
 
 
