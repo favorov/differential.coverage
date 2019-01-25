08858c86adc87243a77afc6b6bb9f3cf6d047842
Alexander Favorov <favorov@sensi.org>
2019-01-25 12:26:33 -0500

    add commnemt... does it make sense to prepare the data???

--- data-raw/ucsc_hg19.R
+++ data-raw/ucsc_hg19.R
@@ -17,6 +17,14 @@ genes<-unlist(genes(TxDb.Hsapiens.UCSC.hg19.knownGene,single.strand.genes.only=F
 #remove strange threads
 genes<-genes[nchar(as.character(seqnames(genes)))<6]
 
+#GRanges object with 2 ranges and 0 metadata columns:
+#  seqnames              ranges strand
+#<Rle>           <IRanges>  <Rle>
+#  96626     chr2 110656009-110664033      +
+#  96626     chr2 111222628-111230652      -
+#  -------
+#  seqinfo: 93 sequences (1 circular) from hg19 genome
+
 
 
 
