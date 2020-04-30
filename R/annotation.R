98ccca66b558d6fbad7f2b9b922962c44464387f
Alexander Favorov <favorov@sensi.org>
2020-04-30 19:54:24 -0400

    changed the names of the rda files in /data

--- R/annotation.R
+++ R/annotation.R
@@ -25,11 +25,11 @@
 get.Known.Gene.List<-function(genome.annotation.id='gencode19',single.strand.genes.only=FALSE)
 {
 	if (genome.annotation.id=='gencode19' || genome.annotation.id=='gencode.19' || genome.annotation.id=='gencode.hg.19')
-		return(gencode19_genes) # it was lazy
+		return(gencode_hs19_genes) # it was lazy
 	if (genome.annotation.id=='gencode26' || genome.annotation.id=='gencode.26' || genome.annotation.id=='gencode.hg.26')
-		return(gencode26_genes) # it was lazy
+		return(gencode_hs26_genes) # it was lazy
 	if (genome.annotation.id=='gencode29' || genome.annotation.id=='gencode.29' || genome.annotation.id=='gencode.hg.29')
-		return(gencode29_genes) # it was lazy
+		return(gencode_hs29_genes) # it was lazy
 	
 	#prepare genes names; we refer the TxDb object by name
 
