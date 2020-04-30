0d978cdd607173a269a0c831dba96ce533c2bb32
Alexander Favorov <favorov@sensi.org>
2020-04-30 19:51:34 -0400

    changed the names of the rda files in /data

---
+++ data-raw/gencode_hs19.R
@@ -0,0 +1,25 @@
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(rtracklayer)
+library(dplyr)
+library(usethis)
+
+if(! 'raw_differtial_coverage_gencode_hs19_gff' %in% ls()){
+  raw_differtial_coverage_gencode_hs19_gff=
+      readGFF("http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gff3.gz",
+              version=3)
+}
+# we use UCSC seqinfo from TxDb.Hsapiens.UCSC.hg19.knownGene
+seqs<-seqinfo(TxDb.Hsapiens.UCSC.hg19.knownGene)
+seqnms<-seqnames(seqs) #all names
+seqnms<-seqnms[nchar(seqnms)<6] #only real sequence names
+seqs<-seqs[seqnms] #seqinfo is subesettable only by names, we make useful chromosome list
+gencode_hs19_genes<-
+  GRanges(as.data.frame(raw_differtial_coverage_gencode_hs19_gff) %>% 
+      filter(type=="gene") %>% 
+      select(seqid,start,end,strand,gene_name,ensembl=gene_id),
+  seqinfo = seqs)
+#gene_id here is ensembl name, but withe additional .n (transcript#), we remove it
+gencode_hs19_genes$ensembl<-sapply(strsplit(gencode_hs19_genes$ensembl,'.',fixed = TRUE),"[[",1)
+
+#we are in the package root folder
+usethis::use_data(gencode_hs19_genes,overwrite = TRUE)
--- data-raw/gencode_hs26.R
+++ data-raw/gencode_hs26.R
@@ -3,24 +3,24 @@ library(rtracklayer)
 library(dplyr)
 library(usethis)
 
-if(! 'raw_differtial_coverage_gencode_29_gff' %in% ls()){
-  raw_differtial_coverage_gencode_29_gff=
-      readGFF("http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.annotation.gff3.gz",
+if(! 'raw_differtial_coverage_gencode_hs26_gff' %in% ls()){
+  raw_differtial_coverage_gencode_hs26_gff=
+      readGFF("http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_26/gencode.v26.annotation.gff3.gz",
               version=3)
 }
 
-  # we use UCSC seqinfo from TxDb.Hsapiens.UCSC.hg19.knownGene
+# we use UCSC seqinfo from TxDb.Hsapiens.UCSC.hg38.knownGene
 seqs<-seqinfo(TxDb.Hsapiens.UCSC.hg38.knownGene)
 seqnms<-seqnames(seqs) #all names
 seqnms<-seqnms[nchar(seqnms)<6] #only real sequence names
 seqs<-seqs[seqnms] #seqinfo is subesettable only by names, we make useful chromosome list
-gencode29_genes<-
-  GRanges(as.data.frame(raw_differtial_coverage_gencode_29_gff) %>% 
+gencode_hs26_genes<-
+  GRanges(as.data.frame(raw_differtial_coverage_gencode_hs26_gff) %>% 
       filter(type=="gene") %>% 
       select(seqid,start,end,strand,gene_name,ensembl=gene_id),
   seqinfo = seqs)
 #gene_id here is ensembl name, but withe additional .n (transcript#), we remove it
-gencode29_genes$ensembl<-sapply(strsplit(gencode29_genes$ensembl,'.',fixed = TRUE),"[[",1)
+gencode_hs26_genes$ensembl<-sapply(strsplit(gencode_hs26_genes$ensembl,'.',fixed = TRUE),"[[",1)
 
 #we are in the package root folder
-usethis::use_data(gencode29_genes,overwrite = TRUE)
+usethis::use_data(gencode_hs26_genes,overwrite = TRUE)
